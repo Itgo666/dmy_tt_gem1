37314fd9441186620ce05ed50b893c3d7758cf4d
yangk7675 <yangk7675@sina.com>
2019-12-23 18:10:50 +0800

    my first gem

---
+++ lib/dmy_tt_gem1.rb
@@ -0,0 +1,11 @@
+require "dmy_tt_gem1/version"
+
+module DmyTtGem1
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def self.itgo
+  	"itgo666"
+  end
+
+end
---
+++ lib/dmy_tt_gem1/version.rb
@@ -0,0 +1,3 @@
+module DmyTtGem1
+  VERSION = "0.1.0"
+end
